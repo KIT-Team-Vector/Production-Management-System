2307ba72119118fd8551f08ffe439148a45221b2
Tobin Zühlke <utkdm@student.kit.edu>
2024-01-29 16:42:18 +0100

    Fixed incorrect groupId's

--- inventory-management/rest-service/src/main/java/assets/RessourceSetAsset.java
+++ inventory-management/rest-service/src/main/java/assets/RessourceSetAsset.java
@@ -5,7 +5,6 @@ import domain.ResourceSet;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
