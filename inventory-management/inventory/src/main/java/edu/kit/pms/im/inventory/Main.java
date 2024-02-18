3d452e3bc9cf6e6dbc2b09eec795b461e42a9008
Tobin Zühlke <utkdm@student.kit.edu>
2024-02-18 13:40:26 +0100

    added temporary application build logic

--- inventory-management/inventory/src/main/java/edu/kit/pms/im/inventory/Main.java
+++ inventory-management/inventory/src/main/java/edu/kit/pms/im/inventory/Main.java
@@ -1,33 +1,57 @@
 package edu.kit.pms.im.inventory;
 
-import java.io.File;
+
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Properties;
+
+
+import java.time.Duration;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutionException;
 
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.Context;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.BooleanDeserializer;
 import org.apache.kafka.common.serialization.BooleanSerializer;
+import org.apache.tomcat.util.scan.StandardJarScanner;
 
+import edu.kit.pms.im.common.concepts.ResourceImpl;
+import edu.kit.pms.im.common.concepts.ResourceSetImpl;
+import edu.kit.pms.im.domain.Resource;
+import edu.kit.pms.im.domain.ResourceSet;
+import edu.kit.pms.im.domain.ResourceSetRepository;
+import edu.kit.pms.im.domain.MicroserviceError;
+
+import edu.kit.pms.im.common.services.MessageReceiverService;
 import edu.kit.pms.im.database.*;
-import edu.kit.pms.im.domain.*;
+
 import edu.kit.pms.im.inventory.resourceLoader.WarLoader;
 import edu.kit.pms.im.message.handlers.*;
+import edu.kit.pms.im.message.kafka.IKafkaConstants;
 import edu.kit.pms.im.message.kafka.clients.ConsumerFactory;
 import edu.kit.pms.im.message.kafka.clients.ProducerFactory;
 import edu.kit.pms.im.message.kafka.clients.SimpleConsumerFactory;
 import edu.kit.pms.im.message.kafka.clients.SimpleProducerFactory;
 import edu.kit.pms.im.message.serialization.MicroserviceErrorSerializer;
-import edu.kit.pms.im.message.serialization.RessourceSetDeserializer;
+import edu.kit.pms.im.message.serialization.ResourceSetDeserializer;
+import edu.kit.pms.im.message.serialization.ResourceSetSerializer;
 import edu.kit.pms.im.message.services.*;
+import edu.kit.pms.im.common.services.*;
+import edu.kit.pms.im.common.controllers.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		ResourceSet a = new ResourceSetImpl(null, 3);
+		Resource b = new ResourceImpl(1, "ww");
 		try {
-			testTomcat();
+			test();
+			startAll();
+			// testProducerKafkaUpdateRequest();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,44 +59,24 @@ public class Main {
 
 	}
 
-	private static void testTomcat() throws IOException {
+	private static void test() {
+		Thread serverThread = new Thread(() -> {
+			// listen for rest requests
+			ConsumerFactory<Boolean, BooleanDeserializer> consumerFactory = new SimpleConsumerFactory<Boolean, BooleanDeserializer>(
+					BooleanDeserializer.class);
+			org.apache.kafka.clients.consumer.Consumer<Long, Boolean> consumer = consumerFactory
+					.create(IKafkaConstants.TOPIC_CHANGE_AMOUNT_OF_RESOURCE_FROM_INVENTORY_RESPONSE);
+			while (true) {
+				ConsumerRecords<Long, Boolean> t = consumer.poll(Duration.ofMillis(1000));
+				t.forEach(record -> {
+					System.out.println(record.value().booleanValue());
 
-		try {
-			// Create an instance of Tomcat
-			Tomcat tomcat = new Tomcat();
-			tomcat.setPort(8080); // Set the port number
+				});
 
-			File warfilee = new File("../rest-service/target/rest-service.war");
-			WarLoader loader = new WarLoader();
-			warfilee  = loader.getWarFile();
-			System.out.println(warfilee.getCanonicalPath());
-			String contextPath = "rest-service"; // Context path for your app
-													// with
-			tomcat.setBaseDir("temp"); // your
-			// actual
-			// path
-
-			tomcat.getHost().setAppBase(".");
-			tomcat.getConnector();
-			tomcat.addWebapp(contextPath, warfilee.getCanonicalPath());
-			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-				try {
-					tomcat.stop();
-				} catch (LifecycleException e) {
-					e.printStackTrace();
-				}
-			}));
-			// Start Tomcat
-			tomcat.start();
-			tomcat.getServer().await();
-		} catch (LifecycleException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		}
+			}
+		});
+		serverThread.start();
 	}
-
-	
 
 	private void testDataBase() {
 		ResourceSetRepository databaseConnector = new SqlResourceSetRepository();
@@ -82,25 +86,130 @@ public class Main {
 		databaseConnector.updateAmount(1, 17);
 	}
 
-	private void testKafka() {
+	private static void testProducerKafkaUpdateRequest() {
+		Timer timer = new Timer();
 
-		// listen and handle kafka requestsc
-		ConsumerFactory<ResourceSet, RessourceSetDeserializer> consumerFactory = new SimpleConsumerFactory<ResourceSet, RessourceSetDeserializer>(
-				RessourceSetDeserializer.class);
-		MessageHandler removeMessageHandler = new RemoveResourcesMessageHandler(consumerFactory);
-		MessageReceiverService ks = new MessageReceiverServiceImpl(removeMessageHandler);
-		Thread thread = new Thread(ks);
-		thread.start();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				Producer<Long, ResourceSetImpl> updateProducer = new SimpleProducerFactory<ResourceSetImpl, ResourceSetSerializer>(
+						ResourceSetSerializer.class).create();
+				ProducerRecord<Long, ResourceSetImpl> record = new ProducerRecord<Long, ResourceSetImpl>(
+						IKafkaConstants.TOPIC_CHANGE_AMOUNT_OF_RESOURCE_FROM_INVENTORY, 1L,
+						new ResourceSetImpl(new ResourceImpl(1, "Test"), 2));
+				try {
+					updateProducer.send(record).get();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}, 10000); // 3000 milliseconds delay (3 seconds)
 
-		// send kafka Request
+	}
+
+	private static void startAll() throws IOException {
+		ResourceSetRepository resourceSetRepository = new SqlResourceSetRepository();
+		InventoryManager inventoryManager = new InventoryManagerImpl(resourceSetRepository);
+
 		ProducerFactory<MicroserviceError, MicroserviceErrorSerializer> errorProducer = new SimpleProducerFactory<MicroserviceError, MicroserviceErrorSerializer>(
 				MicroserviceErrorSerializer.class);
 		ProducerFactory<Boolean, BooleanSerializer> booleanProducer = new SimpleProducerFactory<Boolean, BooleanSerializer>(
 				BooleanSerializer.class);
-		MessageSenderService ms = new MessageSenderServiceImpl(errorProducer, booleanProducer);
-		ms.sendDeleteFromInventoryResponse(false);
-		ms.sendError(new MicroserviceError("testblabla", 32323));
+		MessageSenderService messageSenderService = new MessageSenderServiceImpl(errorProducer, booleanProducer);
 
+		MessageAndRestController.init(inventoryManager, messageSenderService);
+		ExternalInterfaceController endPointController = MessageAndRestController.getInstance();
+
+		// listen and handle kafka requestsc
+		ConsumerFactory<ResourceSetImpl, ResourceSetDeserializer> consumerFactory = new SimpleConsumerFactory<ResourceSetImpl, ResourceSetDeserializer>(
+				ResourceSetDeserializer.class);
+		MessageHandler changeAmountOfResourceMessageHandler = new ChangeAmountOfResourceHandler(endPointController,
+				consumerFactory);
+		MessageReceiverService ks = new MessageReceiverServiceImpl(changeAmountOfResourceMessageHandler);
+		Thread thread = new Thread(ks);
+		ClassLoader mainClassLoader = Main.class.getClassLoader();
+		
+		Thread serverThread = new Thread(() -> {
+			// listen for rest requests
+			try {
+				// Create an instance of Tomcat
+				Tomcat tomcat = new Tomcat();
+				// Set the port number
+				tomcat.setPort(8080);
+				// specify workspace of tomcat instance
+				tomcat.setBaseDir("temp");
+				// root is set to host
+				tomcat.getHost().setAppBase(".");
+				tomcat.getConnector();
+				// host url + contextPath => path to service
+				String contextPath = "rest-service";
+				// load web service war
+				WarLoader loader = new WarLoader();
+				// feed tomcat server with jakarta web service and context path
+				Context ctx = tomcat.addWebapp(contextPath, loader.getWarFile().getCanonicalPath());
+				// add dependecies of main application to jakarta web service (shared
+				// dependencies)
+				ctx.setParentClassLoader(mainClassLoader);
+				// disable all scanns, all tomcat dependecies are already included in the
+				// classpath of the jar
+				StandardJarScanner jarScanner = (StandardJarScanner) ctx.getJarScanner();
+				jarScanner.setScanAllDirectories(false);
+				jarScanner.setScanClassPath(false);
+
+				// gracefull shut down
+				Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+					try {
+						tomcat.stop();
+					} catch (LifecycleException e) {
+						e.printStackTrace();
+					}
+				}));
+				// Start Tomcat
+				tomcat.start();
+				tomcat.getServer().await();
+			} catch (LifecycleException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		});
+		thread.start();
+		serverThread.start();
+	}
+
+	public void kafka() {
+		ResourceSetRepository resourceSetRepository = new SqlResourceSetRepository();
+		InventoryManager inventoryManager = new InventoryManagerImpl(resourceSetRepository);
+
+		ProducerFactory<MicroserviceError, MicroserviceErrorSerializer> errorProducer = new SimpleProducerFactory<MicroserviceError, MicroserviceErrorSerializer>(
+				MicroserviceErrorSerializer.class);
+		ProducerFactory<Boolean, BooleanSerializer> booleanProducer = new SimpleProducerFactory<Boolean, BooleanSerializer>(
+				BooleanSerializer.class);
+		MessageSenderService messageSenderService = new MessageSenderServiceImpl(errorProducer, booleanProducer);
+
+		MessageAndRestController.init(inventoryManager, messageSenderService);
+		ExternalInterfaceController endPointController = MessageAndRestController.getInstance();
+
+		// listen and handle kafka requestsc
+		ConsumerFactory<ResourceSetImpl, ResourceSetDeserializer> consumerFactory = new SimpleConsumerFactory<ResourceSetImpl, ResourceSetDeserializer>(
+				ResourceSetDeserializer.class);
+		MessageHandler changeAmountOfResourceMessageHandler = new ChangeAmountOfResourceHandler(endPointController,
+				consumerFactory);
+		MessageReceiverService ks = new MessageReceiverServiceImpl(changeAmountOfResourceMessageHandler);
+		Thread thread = new Thread(ks);
+		thread.start();
+
+		// send kafka Request
+		messageSenderService.sendChangeAmountOfResourceResponse(4L, true);
+		messageSenderService.sendError(6L, new MicroserviceError("testblabla", "32323"));
 	}
 
 }
--- inventory-management/inventory/src/main/java/edu/kit/pms/im/inventory/resourceLoader/WarLoader.java
+++ inventory-management/inventory/src/main/java/edu/kit/pms/im/inventory/resourceLoader/WarLoader.java
@@ -1,14 +1,16 @@
 package edu.kit.pms.im.inventory.resourceLoader;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 
 public class WarLoader {
-	
+	/**
 	public File getWarFile() {
 		//ClassName>.class.getClassLoader()
-		ClassLoader classLoader = getClass().getClassLoader();
 		URL warURL = null;
 		try {
 			warURL = WarLoader.class.getResource("rest-service.war");
@@ -22,5 +24,29 @@ public class WarLoader {
 			e.printStackTrace();
 			return null;
 		}
-	}
+	}**/
+	
+	public File getWarFile() {
+		// Get the current working directory
+        String jarPath = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+
+        // Navigate to the parent directory
+        File parentDirectory = new File(jarPath).getParentFile();
+
+        // Check if the warFiles folder exists in the parent directory
+        File warFilesFolder = new File(parentDirectory, "warFiles");
+        if (!warFilesFolder.exists() || !warFilesFolder.isDirectory()) {
+            throw new IllegalStateException("warFiles folder not found in the parent directory.");
+        }
+
+        // Construct the path to the WAR file
+        File warFile = new File(warFilesFolder, "rest-service.war");
+
+        // Check if the WAR file exists
+        if (!warFile.exists() || !warFile.isFile()) {
+            throw new IllegalStateException("rest-service.war file not found in the warFiles folder.");
+        }
+
+        return warFile;
+    }
 }
