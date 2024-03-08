165b830f91795cc02255c395fe76d68affef6b1f
Atilla Ates <usxoi@student.kit.edu>
2024-03-08 06:04:23 +0100

    Moved to rest-service

--- order-management/rest-service/src/main/java/edu/kit/ordermanager/MainApplication.java
+++ order-management/rest-service/src/main/java/edu/kit/ordermanager/MainApplication.java
@@ -1,8 +1,7 @@
-package edu.kit.ordermanager.rest;
+package edu.kit.ordermanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class MainApplication {
