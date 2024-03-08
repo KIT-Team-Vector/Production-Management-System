cf8b794aa5465be1986228a0aae2b811bf6dc929
Atilla Ates <usxoi@student.kit.edu>
2024-03-08 16:12:40 +0100

    Edited serializer and deserializer in KafkaConsumerConfig and KafkaProducerConfig.

--- order-management/kafka-service/src/main/java/edu/kit/ordermanager/kafka/producer/KafkaProducerConfig.java
+++ order-management/kafka-service/src/main/java/edu/kit/ordermanager/kafka/producer/KafkaProducerConfig.java
@@ -1,19 +1,18 @@
 package edu.kit.ordermanager.kafka.producer;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import edu.kit.ordermanager.entities.ResourceSet;
-import edu.kit.ordermanager.kafka.Greeting;
+import edu.kit.ordermanager.kafka.serializer.ResourceSetSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -25,8 +24,8 @@ public class KafkaProducerConfig {
     public ProducerFactory<Long, ResourceSet> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ResourceSetSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
