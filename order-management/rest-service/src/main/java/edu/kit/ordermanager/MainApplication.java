57567b9f4c48d0e45615531b6b3e801aad0af7b8
Atilla Ates <usxoi@student.kit.edu>
2024-03-08 13:52:58 +0100

    Exclude DataSourceAutoConfiguration.

--- order-management/rest-service/src/main/java/edu/kit/ordermanager/MainApplication.java
+++ order-management/rest-service/src/main/java/edu/kit/ordermanager/MainApplication.java
@@ -2,13 +2,14 @@ package edu.kit.ordermanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
-
     }
 
 }
