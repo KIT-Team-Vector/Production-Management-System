96f63cff75dc47cb9569b5b2e7aa81ae331da663
Atilla Ates <usxoi@student.kit.edu>
2024-03-08 04:16:00 +0100

    Implemented dependency inversion from controller to framework layer.

--- order-management/app/src/main/java/edu/kit/pms/ordermanager/app/PlaceOrderUseCase.java
+++ order-management/app/src/main/java/edu/kit/pms/ordermanager/app/PlaceOrderUseCase.java
@@ -3,31 +3,29 @@ package edu.kit.pms.ordermanager.app;
 import edu.kit.ordermanager.entities.Resource;
 import edu.kit.ordermanager.entities.ResourceSet;
 import edu.kit.ordermanager.entities.Task;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class PlaceOrderUseCase {
 
-    private final String inventoryApiUrl = "http://134.3.17.150:9092/rest-service/inventory/resource/set";
-    private final String machineApiUrl = "http://<host>:<port>/pms/mm/checkMachine";
+    private IRestServiceController restServiceController;
 
-    private final String requiredResourceApiUrl = "http://<host>:<port>/pms/mm/requiredResource";
-
-    private final String startProductionApiUrl = "http://<host>:<port>/pms/mm/produce";
-    public PlaceOrderUseCase() {
-
+    public PlaceOrderUseCase(IRestServiceController restServiceController) {
+        this.restServiceController = restServiceController;
     }
 
     public boolean processOrder(Task order) {
-        int resourceID = order.getResource().getId();
-        RestTemplate inventoryTemplate = new RestTemplate();
-        String url = inventoryApiUrl + "/" + resourceID;
-        ResourceSet resourceSet = inventoryTemplate.getForObject(url, ResourceSet.class);
 
-        if(resourceSet == null) {
+        ResponseEntity<ResourceSet> resourceSetEntity = this.restServiceController.checkInventory(order);
+
+        if(resourceSetEntity.getStatusCode().value() == 404) {
             return false;
         }
 
-        if(resourceSet.getAmount() == order.getAmount()) {
+        ResourceSet resourceSet = resourceSetEntity.getBody();
+
+        if(resourceSet.getAmount() >= order.getAmount()) {
             //TODO abbuchen vom Inventory durch consumen der Schnittstelle
             return true;
         }
@@ -35,22 +33,14 @@ public class PlaceOrderUseCase {
         if(resourceSet.getAmount() < order.getAmount()) {
             int difference = order.getAmount() - resourceSet.getAmount();
             //TODO: Consume API of maschine manager and ask for machine to produce the difference amount of the requested resource
-            RestTemplate machineTemplate = new RestTemplate();
-            url = machineApiUrl + "/" + resourceID;
-            Boolean machineAvailable = machineTemplate.getForObject(url, Boolean.class);
+            boolean machineAvailable = this.restServiceController.checkAvailableMachinces(order.getResource().getId());
 
             if(Boolean.TRUE.equals(machineAvailable)) {
-                RestTemplate requiredResourceTemplate = new RestTemplate();
-                url = requiredResourceApiUrl + "/" + resourceID;
-                int requiredResourceId = inventoryTemplate.getForObject(url, Integer.class);
-
-                Resource resource = new Resource(resourceID, "Stahl");
+                Resource resource = this.restServiceController.findRequiredResource(order.getResource().getId());
                 Task childTask = new Task(resource, difference);
 
                if (this.processOrder(childTask)) {
-                   RestTemplate startProductionTemplate = new RestTemplate();
-                   url = startProductionApiUrl + "/" + resourceID;
-                   return Boolean.TRUE.equals(startProductionTemplate.getForObject(url, Boolean.class));
+                   return this.restServiceController.startProduction(order.getResource().getId());
                }
             }
         }
---
+++ order-management/rest-service/src/main/java/edu/kit/ordermanager/rest/controller/OrderManagerController.java
@@ -0,0 +1,41 @@
+package edu.kit.ordermanager.rest.controller;
+
+import edu.kit.ordermanager.controllers.RestServiceController;
+import edu.kit.ordermanager.entities.Resource;
+import edu.kit.ordermanager.entities.Task;
+import edu.kit.ordermanager.kafka.handler.MessageHandler;
+import edu.kit.pms.ordermanager.app.PlaceOrderUseCase;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderManagerController {
+    private PlaceOrderUseCase placeOrder;
+
+    private MessageHandler messageHandler;
+
+    public OrderManagerController() {
+        messageHandler = new MessageHandler();
+        RestServiceController restServiceController = new RestServiceController(messageHandler);
+        placeOrder = new PlaceOrderUseCase(restServiceController);
+
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "Hello i am the new edited";
+    }
+    /*public List<Task> index() {
+        return this.taskRepository.findAll();
+    }*/
+
+    @GetMapping("/place")
+    public void placeOrder(@RequestParam(value = "id") int id, @RequestParam(value = "name") String name, @RequestParam(value = "amount") int amount) {
+        Resource resource = new Resource(id, name);
+        Task order = new Task(resource, amount);
+        placeOrder.processOrder(order);
+    }
+}
