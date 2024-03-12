075c765449e634aff038eda8a0356edf801504cd
Atilla Ates <usxoi@student.kit.edu>
2024-03-12 23:26:40 +0100

    Implemented RestServiceControllerTest.

--- order-management/adapters/src/main/java/edu/kit/ordermanager/controllers/RestServiceController.java
+++ order-management/adapters/src/main/java/edu/kit/ordermanager/controllers/RestServiceController.java
@@ -4,12 +4,15 @@ import edu.kit.ordermanager.entities.Resource;
 import edu.kit.ordermanager.entities.ResourceSet;
 import edu.kit.ordermanager.entities.Task;
 import edu.kit.pms.ordermanager.app.IRestServiceController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class RestServiceController implements IRestServiceController {
+
+    RestTemplate restTemplate;
 
     String inventoryServiceHost = System.getenv("INVENTORY_HOST");
 
@@ -18,21 +21,51 @@ public class RestServiceController implements IRestServiceController {
     String machineServiceHost = System.getenv("MACHINE_HOST");
 
     String machineServicePort = System.getenv("MACHINE_PORT");
-    private final String checkInventoryUrl = "http://" + inventoryServiceHost+ ":" +inventoryServicePort + "/rest-service/inventory/resource/set";
-    private final String checkMachineUrl = "http://" + machineServiceHost+ ":" + machineServicePort + "/pms/mm/checkMachine";
+    private String checkInventoryUrl;
+    private String checkMachineUrl;
 
-    private final String requiredResourceUrl = "http://" + machineServiceHost+ ":" + machineServicePort + "/pms/mm/requiredResource";
+    private String requiredResourceUrl;
 
-    private final String startProductionUrl = "http://" + machineServiceHost+ ":" + machineServicePort + "/pms/mm/produce";
+    private String startProductionUrl;
+
+    public RestServiceController() {
+        initUrls();
+    }
+
+    private void initUrls() {
+        inventoryServiceHost = System.getenv("INVENTORY_HOST");
+
+        inventoryServicePort = System.getenv("INVENTORY_PORT");
+
+        machineServiceHost = System.getenv("MACHINE_HOST");
+
+        machineServicePort = System.getenv("MACHINE_PORT");
+
+        checkInventoryUrl = "http://" + inventoryServiceHost+ ":" +inventoryServicePort + "/rest-service/inventory/resource/set";
+
+        checkMachineUrl = "http://" + machineServiceHost+ ":" + machineServicePort + "/pms/mm/checkMachine";
+
+        requiredResourceUrl = "http://" + machineServiceHost+ ":" + machineServicePort + "/pms/mm/requiredResource";
+
+        startProductionUrl = "http://" + machineServiceHost+ ":" + machineServicePort + "/pms/mm/produce";
+    }
+
+    /**
+     * For tests
+     * @param restTemplate
+     */
+    public RestServiceController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+        initUrls();
+    }
 
     @Override
     public ResourceSet checkInventory(Task order) {
         int resourceID = order.getResource().getId();
-        RestTemplate inventoryTemplate = new RestTemplate();
         String url = checkInventoryUrl + "/" + resourceID;
-        ResponseEntity<ResourceSet> responseEntity = inventoryTemplate.getForEntity(url, ResourceSet.class);
+        ResponseEntity<ResourceSet> responseEntity = restTemplate.getForEntity(url, ResourceSet.class);
 
-        if(responseEntity.getStatusCode().value() == 404) {
+        if(responseEntity.getStatusCode() != HttpStatus.ACCEPTED) {
             return null;
         }
 
@@ -41,21 +74,22 @@ public class RestServiceController implements IRestServiceController {
 
     @Override
     public boolean checkAvailableMachinces(int resourceId) {
-        RestTemplate machineTemplate = new RestTemplate();
         String url = checkMachineUrl + "/" + resourceId;
-        return Boolean.TRUE.equals(machineTemplate.getForObject(url, Boolean.class));
+        return Boolean.TRUE.equals(restTemplate.getForObject(url, Boolean.class));
     }
 
     @Override
     public Resource findRequiredResource(int resourceId) {
-        RestTemplate requiredResourceTemplate = new RestTemplate();
         String url = requiredResourceUrl + "/" + resourceId;
-        return requiredResourceTemplate.getForObject(url, Resource.class);
+        return restTemplate.getForObject(url, Resource.class);
     }
 
     @Override
     public boolean startProduction(ResourceSet resourceSet) {
-        RestTemplate startProductionTemplate = new RestTemplate();
-        return Boolean.TRUE.equals(startProductionTemplate.postForObject(startProductionUrl, resourceSet, Boolean.class));
+        return Boolean.TRUE.equals(restTemplate.postForObject(startProductionUrl, resourceSet, Boolean.class));
+    }
+
+    public String getCheckInventoryUrl() {
+        return this.checkInventoryUrl;
     }
 }
---
+++ order-management/adapters/src/test/java/RestServiceControllerTest.java
@@ -0,0 +1,67 @@
+import edu.kit.ordermanager.controllers.RestServiceController;
+import edu.kit.ordermanager.entities.Resource;
+import edu.kit.ordermanager.entities.ResourceSet;
+import edu.kit.ordermanager.entities.Task;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+public class RestServiceControllerTest {
+
+    @Mock
+    RestTemplate inventoryTemplate;
+
+    @InjectMocks
+    private RestServiceController restServiceController;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void testCheckInventory_noErrorCode() {
+        int id = 1;
+        String name = "steel";
+        int amount = 3;
+
+        Resource resource = new Resource(id, name);
+        Task task = new Task(resource, amount);
+
+        ResourceSet resourceSet = new ResourceSet(new Resource(id, name), amount);
+
+        when(inventoryTemplate.getForEntity(any(String.class), eq(ResourceSet.class))).thenReturn(new ResponseEntity<ResourceSet>(resourceSet, HttpStatus.ACCEPTED));
+
+        ResourceSet responseResourceSet = restServiceController.checkInventory(task);
+        assertEquals(resource.getId(), responseResourceSet.getResource().getId());
+        assertEquals(amount, responseResourceSet.getAmount());
+    }
+
+    @Test
+    public void testCheckInventory_noWithErrorCode() {
+        int id = 1;
+        String name = "steel";
+        int amount = 3;
+
+        Resource resource = new Resource(id, name);
+        Task task = new Task(resource, amount);
+
+        ResourceSet resourceSet = new ResourceSet(new Resource(id, name), amount);
+
+        when(inventoryTemplate.getForEntity(any(String.class), eq(ResourceSet.class))).thenReturn(new ResponseEntity<ResourceSet>(resourceSet, HttpStatus.NOT_FOUND));
+
+        ResourceSet responseResourceSet = restServiceController.checkInventory(task);
+        assertNull(responseResourceSet);
+    }
+}
