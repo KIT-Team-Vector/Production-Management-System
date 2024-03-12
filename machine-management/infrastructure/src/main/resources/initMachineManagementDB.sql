dee8910ca84c6d81ace1b85dfde915790539920b
Tim Schilpp <tim.schilpp@gmail.com>
2024-03-12 14:22:28 +0100

    - Fixed Kafka bug leading to consumer missing responses - Fixed init script for the database - Added logging info - Updated dependencies to resolve vulnerabilities

--- machine-management/infrastructure/src/main/resources/initMachineManagementDB.sql
+++ machine-management/infrastructure/src/main/resources/initMachineManagementDB.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS one_to_one_machine
 
 INSERT INTO one_to_one_machine (id, input_resource_id, input_resource_name, output_resource_id, output_resource_name)
 VALUES (1, 1, 'steel', 2, 'wood'),
-       (2, 1, 'steel', 3, 'copper'),
-       (3, 2, 'wood', 4, 'paper'),
-       (4, 3, 'copper', 5, 'plastic'),
-       (5, 4, 'copper', 1, 'steel');
+       (2, 2, 'wood', 3, 'copper'),
+       (3, 3, 'copper', 4, 'paper'),
+       (4, 4, 'paper', 5, 'plastic'),
+       (5, 5, 'plastic', 1, 'steel');
